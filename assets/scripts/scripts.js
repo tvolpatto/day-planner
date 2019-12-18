4fd468867e962b4a2b64195822ff690ca3d3a4a2
Thyago Volpatto <tvolpatto@gmail.com>
2019-12-17 21:48:43 -0700

    refactor rendering function

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -3,37 +3,45 @@ var schedule;
 var firstHour = 9;
 
 function renderTimeBlocks() {
-    for(var i = 0; i < 9; i++) {
+    for (var i = 0; i < 9; i++) {
         var hour = i + firstHour;
-        var timeBLock = $("<div>");
-        timeBLock.addClass("row time-block");
+
+        var timeBLock = $("<div>").addClass("row time-block");
+
         container.append(timeBLock);
-        
-        var colHour = $("<div>");
-        colHour.addClass("hour col-1");
-        colHour.text(hour);
 
-        var colPlanner = $("<div>");
-        colPlanner.addClass("col-10");
-        var textArea = $("<textarea>");
-        colPlanner.append(textArea);
-        textArea.attr("id", `text-${hour}`);
-        if (schedule[i] != null){
-            textArea.val(schedule[i]);
-        }
-    
+        var colHour = createHourCol(hour);
 
-        var colSave = $("<div>");
-        colSave.addClass("col-1");
-        var btnSave = $("<button>Save</button>");
-        btnSave.addClass("saveBtn");
-        btnSave.attr("data-time", hour);
-        colSave.append(btnSave);
+        var colPlanner = createPlannerCol(hour, schedule[i]);
 
+        var colSave = createSaveCol(hour);
 
         timeBLock.append(colHour, colPlanner, colSave);
     }
+}
 
+function createHourCol(hour) {
+    return $("<div>").addClass("hour col-1")
+        .text(hour);
+}
+
+function createPlannerCol(hour, text) {
+    var textArea = $("<textarea>");
+    textArea.attr("id", `text-${hour}`);
+    if (text != null) {
+        textArea.val(text);
+    }
+
+    return $("<div>").addClass("col-10")
+        .append(textArea);
+}
+
+function createSaveCol(hour) {
+    var btnSave = $("<button>Save</button>");
+    btnSave.addClass("saveBtn");
+    btnSave.attr("data-time", hour);  
+
+    return $("<div>").addClass("col-1").append(btnSave);;
 }
 
 function save() {
@@ -46,16 +54,14 @@ function save() {
 }
 
 function load() {
-    schedule = localStorage.getItem("schedule")  != null
-    ? JSON.parse(localStorage.getItem("schedule")) : new Array(9);
+    schedule = localStorage.getItem("schedule") != null
+        ? JSON.parse(localStorage.getItem("schedule")) : new Array(9);
 
 }
 
-
-
-$(document).ready(function(){
+$(document).ready(function () {
     load();
     renderTimeBlocks();
-    container.on("click","button", save);
-    
+    container.on("click", "button", save);
+
 });
