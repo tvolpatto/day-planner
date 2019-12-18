3f0ce9a8fe8a5d56eb5d7d678bbf75c35bb9b842
Thyago Volpatto <tvolpatto@gmail.com>
2019-12-17 21:44:01 -0700

    saving to localStorage

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,36 +1,61 @@
 var container = $(".container");
+var schedule;
+var firstHour = 9;
 
 function renderTimeBlocks() {
-    for(var i = 9; i < 18; i++) {
+    for(var i = 0; i < 9; i++) {
+        var hour = i + firstHour;
         var timeBLock = $("<div>");
         timeBLock.addClass("row time-block");
         container.append(timeBLock);
         
         var colHour = $("<div>");
         colHour.addClass("hour col-1");
-        colHour.text(i);
+        colHour.text(hour);
 
         var colPlanner = $("<div>");
         colPlanner.addClass("col-10");
         var textArea = $("<textarea>");
         colPlanner.append(textArea);
+        textArea.attr("id", `text-${hour}`);
+        if (schedule[i] != null){
+            textArea.val(schedule[i]);
+        }
+    
 
         var colSave = $("<div>");
         colSave.addClass("col-1");
         var btnSave = $("<button>Save</button>");
         btnSave.addClass("saveBtn");
+        btnSave.attr("data-time", hour);
         colSave.append(btnSave);
-        colSave.append(colSave);
 
-        timeBLock.append(colHour, colPlanner, colSave)
+
+        timeBLock.append(colHour, colPlanner, colSave);
     }
+
+}
+
+function save() {
+    var hour = $(this).attr("data-time");
+    var text = $(`#text-${hour}`).val();
+    var index = parseInt(hour) - firstHour;
+    schedule[index] = text;
+
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+}
+
+function load() {
+    schedule = localStorage.getItem("schedule")  != null
+    ? JSON.parse(localStorage.getItem("schedule")) : new Array(9);
 
 }
 
 
 
 $(document).ready(function(){
+    load();
     renderTimeBlocks();
-    
+    container.on("click","button", save);
     
 });
